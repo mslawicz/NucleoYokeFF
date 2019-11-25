b7cd6888f484c76a567349be9bb0198bfb45bb23
Marcin <slawicz@echostar.pl>
2019-11-25 18:38:01 +0100

    ViewAngle class created

--- NucleoYokeFF/ViewAngle.cpp
+++ NucleoYokeFF/ViewAngle.cpp
@@ -1,1 +1,62 @@
 #include "ViewAngle.h"
+
+ViewAngle::ViewAngle(void)
+{
+    currentAngle = 0.0f;
+    requestedAngle = 0.0f;
+}
+
+/*
+* get angle of pilot's view
+    input:
+    -2 set -90 degrees
+    -1 decrease angle (left / down)
+    0
+    1 increase angle (right / up)
+    2 set +90 degrees
+    3 set 0 degrees
+    timeElapsed: time elapsed since the last call in seconds
+    returns view angle in degrees
+*/
+float ViewAngle::getAngle(int movement, float timeElapsed)
+{
+    switch (movement)
+    {
+    case -1:
+        requestedAngle -= timeElapsed * AngleChangeRate;
+        if (requestedAngle > 180.0f)
+        {
+            requestedAngle = 180.0f;
+        }
+        break;
+    case 1:
+        requestedAngle += timeElapsed * AngleChangeRate;
+        if (requestedAngle < -180.0f)
+        {
+            requestedAngle = -180.0f;
+        }
+        break;
+    case -2:
+        requestedAngle = -90.0f;
+        break;
+    case 2:
+        requestedAngle = 90.0f;
+        break;
+    case 3:
+        requestedAngle = 0.0f;
+        break;
+    case 0:
+    default:
+        break;
+    }
+    currentAngle += (requestedAngle - currentAngle) * timeElapsed * SpeedFactor;
+    if (currentAngle > 180.0f)
+    {
+        currentAngle = 180.0f;
+    }
+    else if (currentAngle < -180.0f)
+    {
+        currentAngle = -180.0f;
+    }
+    return currentAngle;
+}
--- NucleoYokeFF/ViewAngle.h
+++ NucleoYokeFF/ViewAngle.h
@@ -1,5 +1,13 @@
 #pragma once
+
 class ViewAngle
 {
+public:
+    ViewAngle(void);
+    float getAngle(int movement, float timeElapsed);
+private:
+    const float AngleChangeRate = 90.0f;		// rate of angle change in deg/s
+    const float SpeedFactor = 7.0f;				// how fast angle should follow the requested angle
+    float currentAngle;		// current calculated angle in degrees
+    float requestedAngle;	// requested angle in degrees
 };
-
